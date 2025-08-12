1112ac09647742264917adaa9790d3e5226265c3
firaSE-16 <loarifse16@gmail.com>
2025-08-12 23:10:32 +0300

    updated

--- src/_components/invite-button.tsx
+++ src/_components/invite-button.tsx
@@ -6,34 +6,42 @@ import { Input } from '~/components/ui/input'
 import useProject from '~/hooks/use-project'
 
 const InviteButton = () => {
-    const {projectId} = useProject()
-    const [open, setOpen] = React.useState(false)
+  const { projectId } = useProject()
+  const [open, setOpen] = React.useState(false)
+  const [inviteUrl, setInviteUrl] = React.useState("")
 
-    return (
-        <>
-            <Dialog open={open} onOpenChange={setOpen}>
-                <DialogContent>
-                    <DialogHeader>
-                        <DialogTitle>Invite Team Members</DialogTitle>
-                    </DialogHeader>
-                    <p className='text-sm text-gray-500'>
-                        Ask them to copy and paste this link
-                    </p>
-                    <Input
-                        className='mt-4'
-                        readOnly
-                        onClick={() => {
-                            navigator.clipboard.writeText(`${window.location.origin}/join#${projectId}`)
-                        }}
-                        value={`${window.location.origin}/join/${projectId}`}
-                    />
-                </DialogContent>
-            </Dialog>
-            <Button size='sm' onClick={()=>setOpen(true)}>
-                Invite Members
-            </Button>
-            </>
-    )
+  React.useEffect(() => {
+    // Only runs in browser
+    if (typeof window !== "undefined" && projectId) {
+      setInviteUrl(`${window.location.origin}/join/${projectId}`)
+    }
+  }, [projectId])
+
+  return (
+    <>
+      <Dialog open={open} onOpenChange={setOpen}>
+        <DialogContent>
+          <DialogHeader>
+            <DialogTitle>Invite Team Members</DialogTitle>
+          </DialogHeader>
+          <p className='text-sm text-gray-500'>
+            Ask them to copy and paste this link
+          </p>
+          <Input
+            className='mt-4'
+            readOnly
+            onClick={() => {
+              if (inviteUrl) navigator.clipboard.writeText(inviteUrl)
+            }}
+            value={inviteUrl}
+          />
+        </DialogContent>
+      </Dialog>
+      <Button size='sm' onClick={() => setOpen(true)}>
+        Invite Members
+      </Button>
+    </>
+  )
 }
 
 export default InviteButton
